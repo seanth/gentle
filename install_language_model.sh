85b2dc7b6a23ccab0adfe432c680c4204ff11087
seanth <seanth@gmail.com>
2020-08-25 22:02:08 -0500

    minor flag change

--- install_language_model.sh
+++ install_language_model.sh
@@ -1,3 +1,3 @@
-wget -c https://lowerquality.com/gentle/aspire-hclg.tar.gz
+wget -c https://lowerquality.com/gentle/aspire-hclg.tar.gz --no-check-certificate
 tar -xzvf aspire-hclg.tar.gz
 rm aspire-hclg.tar.gz
